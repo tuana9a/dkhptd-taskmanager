0b8082d3f8a2c842400069234ab0089182360030
Tuan Nguyen Minh <tuana9a@gmail.com>
2023-03-18 16:29:09 +0700

    add env DATABASE_NAME

--- src/cfg.ts
+++ src/cfg.ts
@@ -10,7 +10,7 @@ export const cfg = {
   LOG_DIR: process.env.LOG_DIR || "./logs",
   RABBITMQ_CONNECTION_STRING: process.env.RABBITMQ_CONNECTION_STRING || "amqp://localhost:5672",
   MONGODB_CONNECTION_STRING: process.env.MONGODB_CONNECTION_STRING || "mongodb://localhost:27017",
-  DATABASE_NAME: "dkhptd",
+  DATABASE_NAME: process.env.DATABASE_NAME || "dkhptd",
   JOB_MAX_TRY: 10,
   LOG_WORKER_DOING: parseInt(process.env.LOG_WORKER_DOING),
   LOG_WORKER_PING: parseInt(process.env.LOG_WORKER_PING),
